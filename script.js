61b9ffeed8de5a6f44d73c32c0ad45d1a9e99859
Armando Endzelis <endzelisp@gmail.com>
2022-08-26 16:02:15 -0400

    Add case insensitive feature

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 function keyCode (key) {
   switch(key) {
     case 'a':
+    case 'A':
       return 65
     case 's':
+    case 'S':
       return 83
     case 'd':
+    case 'D':
       return 68
     case 'f':
+    case 'F':
       return 70
     case 'g':
+    case 'G':
       return 71
     case 'h':
+    case 'H':
       return 72
     case 'j':
+    case 'J':
       return 74
     case 'k':
+    case 'K':
       return 75
     case 'l':
+    case 'L':
       return 76
     default:
       return null
@@ -24,6 +33,7 @@ function keyCode (key) {
 }
 
 function playAudio(e) {
+  console.log(e.keyCode)
   const keyValue = keyCode(e.key)
   const audio = document.querySelector(`audio[data-key="${keyValue}"]`);
   audio.currentTime = 0;
