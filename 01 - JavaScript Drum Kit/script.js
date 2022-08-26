c6ea2b5842a227cf15b964e5dc76206b4e66d12f
Armando Endzelis <endzelisp@gmail.com>
2022-08-25 23:54:58 -0400

    Refactor to apply effect to all keys

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,15 +1,45 @@
-const divKeysNodeList = document.querySelectorAll('.key');
+function keyCode (key) {
+  switch(key) {
+    case 'a':
+      return 65
+    case 's':
+      return 83
+    case 'd':
+      return 68
+    case 'f':
+      return 70
+    case 'g':
+      return 71
+    case 'h':
+      return 72
+    case 'j':
+      return 74
+    case 'k':
+      return 75
+    case 'l':
+      return 76
+    default:
+      return null
+  }
+}
 
+function playAudio(e) {
+  const keyValue = keyCode(e.key)
+  const audio = document.querySelector(`audio[data-key="${keyValue}"]`);
+  audio.currentTime = 0;
+  audio.play();
+};
 
-divKeysNodeList.forEach(item => {
-  let keyValue = item.getAttribute('data-key');
-  addEventListener('keydown', (e) => {
-    if (keyValue === '65' && e.key === 'a') {
-      item.classList.add('playing')
-      document.querySelector('audio[data-key="65"]').play()
-      addEventListener('keyup', (e) => {
-        item.classList.remove('playing')
-      })
-    }
+function addEffect(e) {
+  const keyValue = keyCode(e.key)
+  const keyLight = document.querySelector(`div[data-key="${keyValue}"]`);
+  keyLight.classList.add('playing')
+  addEventListener('keyup', () => {
+    keyLight.classList.remove('playing')
   })
-})
+}
+
+window.addEventListener('keydown', (e) => {
+  playAudio(e)
+  addEffect(e)
+});
