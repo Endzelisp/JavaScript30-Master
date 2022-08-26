123904d3b4a3d5ae6b5dedb7e02a0950ce4fe908
Armando Endzelis <endzelisp@gmail.com>
2022-08-25 22:18:34 -0400

    Improve event listener and add audio

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,18 +1,15 @@
 const divKeysNodeList = document.querySelectorAll('.key');
 
-const keyPressed = () => {
-  document.addEventListener('keydown', (e) => {
-    switch (e.key) {
-      case 'a':
-        document.querySelector('#a').setAttribute('class', 'playing');
-        document.addEventListener('keyup', (e) => {
-          document.querySelector('#a').setAttribute('class', 'key');
-        })
-        break;
-      case 's':
-        console.log('S pressed')
-    }
-  });
-};
 
-keyPressed()
+divKeysNodeList.forEach(item => {
+  let keyValue = item.getAttribute('data-key');
+  addEventListener('keydown', (e) => {
+    if (keyValue === '65' && e.key === 'a') {
+      item.classList.add('playing')
+      document.querySelector('audio[data-key="65"]').play()
+      addEventListener('keyup', (e) => {
+        item.classList.remove('playing')
+      })
+    }
+  })
+})
